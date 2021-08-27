e052adc9dc47313b4bbdfbeacac28905a034a20e
Chigozirim <iamokwukwe@gmail.com>
2021-08-27 13:50:48 -0700

    Change code

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -2,15 +2,15 @@
 #include <stdlib.h>
 #include <time.h>
 /**
- * *main - main block
- * *prints all the numbers of base 16 in lowercase
- * *Return: 0
- * */
+ *main - main block
+ *prints all the numbers of base 16 in lowercase
+ *Return: 0
+ */
 int main(void)
 {
 	int d;
-	        for (d = 0; d <= 15; d++)
-			        purchar(d + '0');
-		        purchar('\n');
-			return (0);
+	for (d = 0; d <= 15; d++)
+		purchar(d + '0');
+	purchar('\n');
+	return (0);
 }
