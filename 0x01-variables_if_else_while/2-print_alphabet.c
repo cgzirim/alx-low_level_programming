21f8d66f573a5329ecd8a4a3bb46c6c677b51923
Chigozirim <iamokwukwe@gmail.com>
2021-08-27 13:04:25 -0700

    Correct code

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -9,11 +9,8 @@
 int main(void)
 {
 	char ch = 'a';
-	for (ch = 'a'; ch <= 'z'; ch++)
-		
+	        for (ch = 'a'; ch <= 'z'; ch++)
 		putchar(ch);
-
-	putchar('\n');
-
+		putchar('\n');
 	return (0);
 }
