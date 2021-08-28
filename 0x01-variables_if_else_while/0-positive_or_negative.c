cca9df87dec7de00951565cf715e9ce238a2d59b
Chigozirim <iamokwukwe@gmail.com>
2021-08-28 09:21:33 -0700

    Correct code

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -13,15 +13,15 @@ int main(void)
 
 	if (n > 0)
 	{
-		printf("is positive")
+		printf("is positive");
 	}
 	if (n == 0)
 	{
-		printf("is zero")
+		printf("is zero");
 	}
 	if (n < 0)
 	{
-		printf("is negative")
+		printf("is negative");
 	}
 	return (0);
 }
