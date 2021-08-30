7bc957402ac629c190f6b37f528d10d020cfdf4a
Chigozirim Igweamaka <iamokwukwe@gmail.com>
2021-08-30 10:19:03 -0700

    Meet betty's requirement

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -6,9 +6,9 @@
  */
 int main(void)
 {
-	char ch = 'a';
-	for (ch = 'a'; ch <= 'z'; ch++)
-		putchar(ch);
-	putchar('\n');
-	return (0);
+char ch = 'a';
+for (ch = 'a'; ch <= 'z'; ch++)
+putchar(ch);
+putchar('\n');
+return (0);
 }
