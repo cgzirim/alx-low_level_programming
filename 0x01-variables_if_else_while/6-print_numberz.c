2ecf661103030e3b94fed15f18aa54c6859a6c98
Chigozirim Igweamaka <iamokwukwe@gmail.com>
2021-08-30 07:25:26 -0700

    Output number 1-9 with putchar

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,14 +1,15 @@
 #include <stdio.h>
 /**
  * main - main Block
- * output number 0-9 
+ * output number 0-9
  * Return: 0
  */
 int main(void)
 {
 int num;
+char a
 for (num = 0; num <= 9; num++)
-putchar(num);
+a = num, putchar('num');
 putchar('\n');
 return (0);
 }
