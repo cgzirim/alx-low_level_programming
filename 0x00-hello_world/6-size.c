d63f8785428beab4f0e79dd4bc344f1b8829de21
Chigozirim Igweamaka <iamokwukwe@gmail.com>
2021-08-30 11:08:03 -0700

    Complete tasks

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,15 +1,22 @@
+
 #include <stdio.h>
+
 /**
- * main - prints the size of various types on a computer
+ *main - Entry point
  *
- *Return: Always 0
+ *Return: Always 0 (Success)
  */
 int main(void)
 {
-fprintf(stderr, "Size of a char: %2d byte(s)\n", sizeof(char));
-fprintf(stderr, "Size of an int: %2d byte(s)\n", sizeof(int));
-fprintf(stderr, "Size of a long int: %2d byte(s)\n", sizeof(long int));
-fprintf(stderr, "Size of a long long int: %2d byte(s)\n", sizeof(long long int));
-fprintf(stderr, "Size of a float: %2d byte(s)\n", sizeof(float));
-	return (0);
+int a;
+long int b;
+long long int c;
+char d;
+float f;
+printf("Size of a char: %d byte(s)\n", sizeof(d));
+printf("Size of an int: %d byte(s)\n", sizeof(a));
+printf("Size of a long int: %d byte(s)\n", sizeof(b));
+printf("Size of a long long int: %d byte(s)\n", sizeof(c));
+printf("Size of a float: %d byte(s)\n", sizeof(f));
+return (0);
 }
