d13978cabd9e822d14e03529ca57d810000ee33d
Chigozirim Igweamaka <iamokwukwe@gmail.com>
2021-08-31 12:12:21 -0700

    Correct file 5

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -2,7 +2,7 @@
 /**
  * print_sign - Output based on number sign
  * Return: 1 if number is positive, -1 if negative * and 0 if zero
- */ 
+ */
 int print_sign(int n)
 {
 if (n > 0)
@@ -12,11 +12,12 @@ return (1);
 }
 else if (n < 0)
 {
-_putchar('_');
+_putchar('-');
 return (-1);
 }
 else
 {
+_putchar('0');
 return (0);
 }
 }
