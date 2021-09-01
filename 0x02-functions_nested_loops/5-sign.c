58e7e200aa4693ec7102a15a8cd0d54c3d46c307
Chigozirim Igweamaka <iamokwukwe@gmail.com>
2021-09-01 00:48:49 -0700

    Redo file 5-sign.c

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_sign - Print output based on integrr sign
+ * @n: Parrameter that takes an argument of type int
+ *Return: return 1 if integer sign is positive, -1 if negative and 0 if neutral
+ */
+int print_sign(int n)
+{
+if (n > 0)
+_putchar('+');
+return (1);
+else if (n < 0)
+_putchar('-');
+return (1)
+else
+return (0);
+}
