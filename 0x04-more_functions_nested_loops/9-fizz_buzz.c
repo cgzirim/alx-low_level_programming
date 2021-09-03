ebc38f464160fa0d01fe118fe89bcc8a504e4b4e
Igweamaka Chigozirim <iamokwukwe@gmail.com>
2021-09-03 16:30:04 -0700

    Correct file 8

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -9,12 +9,12 @@ int main(void)
 int n;
 for (n = 1; n <= 100; n++)
 {
-if ((n % 3) == 0)
-printf("Fizz");
+if ((n % 15) == 0)
+printf("FizzBuzz");
 else if ((n % 5) == 0)
 printf("Buzz");
-else if ((n % 15) == 0)
-printf("FizzBuzz");
+else if ((n % 3) == 0)
+printf("Fizz");
 else
 printf("%d ", n);
 if (n < 100)
