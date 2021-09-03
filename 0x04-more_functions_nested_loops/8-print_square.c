d31b643aa9810eb510816f3a03cc5b9413ba200f
Igweamaka Chigozirim <iamokwukwe@gmail.com>
2021-09-03 14:38:51 -0700

    Correct file

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -1,1 +1,23 @@
-shdo
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_square - prints a square of size size
+ * @size: size of square
+ *
+ * Return: void
+ */
+void pribt_square(int size)
+{
+int i, j;
+for (i = 0; i <= size; i++)
+{
+if (size >  0)
+{
+for (j = 0; j <= size; j++)
+{
+putchar('#');
+putchar('\n');
+}
+}
+}
+}
