933e98e83a6ca08d146d9469cacd14ef3db79b8f
Igweamaka Chigozirim <iamokwukwe@gmail.com>
2021-09-03 09:09:17 -0700

    Correct code in file 5

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include <stdio.h>
 /**
  * more_numbers - output 10 times the numbers, from 0 to 14
  * Return: Always 0.
@@ -8,20 +7,20 @@ void more_numbers(void)
 {
 int n;
 int i;
-for (n = 0; n <= 9; n++)
+for (n = 0; n <= 10; n++)
 {
-for (i = 0; i <= 14; i++)
+for (i = 0; i <= 15; i++)
 {
 if (i >= 10)
 {
-putchar(i / 10 + '0');
-putchar(i % 10 + '0');
+_putchar(i / 10 + '0');
+_putchar(i % 10 + '0');
 }
 else
 {
-putchar(i + '0');
+_putchar(i + '0');
 }
 }
-putchar('\n');
+_putchar('\n');
 }
 }
