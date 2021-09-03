ba88eec744bc6eba26fdb838bf3847e3de05f82e
Igweamaka Chigozirim <iamokwukwe@gmail.com>
2021-09-03 09:00:42 -0700

    Correct code in file 5

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -12,12 +12,15 @@ for (n = 0; n <= 10; n++)
 {
 for (i = 0; i <= 14; i++)
 {
-if(i >= 10)
+if (i >= 10)
 {
 putchar(i / 10 + '0');
 putchar(i % 10 + '0');
 }
+else
+{
 putchar(i + '0');
+}
 }
 putchar('\n');
 }
