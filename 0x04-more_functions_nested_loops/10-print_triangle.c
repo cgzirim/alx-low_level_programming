8d682535663a1debbd67df6d8b47b53f88fa65ec
Igweamaka Chigozirim <iamokwukwe@gmail.com>
2021-09-04 05:06:19 -0700

    Correct file 10

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,36 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_triangle - Outputs triangle in with the # sign
+ * @size: Collect an integet for size of the tiangle
+ */
+void print_triangle(int size)
+{
+int i, j, k, l;
+int b = size;
+/* Print newline if size is <= zero */
+if (size <= 0)
+{
+putchar(' ');
+putchar('\n');
+}
+/* Loop that handles length of the tiangle */
+for (i = 0; i < size; i++)
+{
+b--;
+/* Loop that handles width of the triangle */
+for (j = 0; j < 1; j++)
+{
+/* Loops that define number of space and # on a line */
+for (k = 0; k <= b; k++)
+{
+putchar(' ');
+}
+for (l = 0; l <= i; l++)
+{
+putchar('#');
+}
+putchar('\n');
+}
+}
+}
