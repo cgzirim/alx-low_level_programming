8a16853005c78463f310c16f59a23892778ce14f
Igweamaka Chigozirim <iamokwukwe@gmail.com>
2021-09-06 12:26:31 -0700

    Correct file 5

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -2,7 +2,7 @@
 #include <string.h>
 #include "main.h"
 /**
- * print_rev - outputs reverse of a string
+ * rev_string - outputs reverse of a string
  * @s: collect string
  */
 void rev_string(char *s)
