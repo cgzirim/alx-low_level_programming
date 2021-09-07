0664c3a6af477fd7a97c634e4ed8aa6519b7a135
Igweamaka Chigozirim <iamokwukwe@gmail.com>
2021-09-06 21:33:53 -0700

    Complete code

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -9,5 +9,5 @@ void print_diagonal(int n);
 void print_square(int size);
 void print_triangle(int size);
 void _putchar(char c);
-void print_number(void);
+void print_number(int n);
 void print_integer(int m);
