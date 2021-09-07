70ca73922c9f4a3bc5e6a6351739bf250308bb47
Igweamaka Chigozirim <iamokwukwe@gmail.com>
2021-09-06 21:11:13 -0700

    Complete code

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -2,10 +2,17 @@
 #include "main.h"
 /**
  * main - check the code
- *
  * Return: Always 0.
  */
 void print_number(int n)
 {
-  putchar(n + '0');
+if (n == 0)
+_putchar('0');
+else if (n < 0)
+{
+_putchar('-');
+print_integer(n * -1);
+}
+else
+print_integer(n);
 }
