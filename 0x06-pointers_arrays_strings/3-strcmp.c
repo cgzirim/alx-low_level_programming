75f213f51c2018acfd0ff0d33d847b1ba36a5adf
Igweamaka Chigozirim <iamokwukwe@gmail.com>
2021-09-09 02:27:06 -0700

    Correct code for file 3

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -8,14 +8,14 @@
  */
 int _strcmp(char *s1, char *s2)
 {
-	int i;
-	for(i = 0; s1[i] != '\0' || s2[i] != '\0'; i++)
-	{
-		if(s1[i] == s2[i])
-			return (0);
-		else
-			return(*s1 - *s2);
-	}
-	return (0);
+int i;
+for (i = 0; s1[i] != '\0' || s2[i] != '\0'; i++)
+{
+if (s1[i] == s2[i])
+return (0);
+else
+return (*s1 - *s2);
+}
+return (0);
 }
 
