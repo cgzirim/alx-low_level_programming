6b6f537a3c57bc890423e4f2a791322f48b2821f
Igweamaka Chigozirim <iamokwukwe@gmail.com>
2021-09-09 03:01:11 -0700

    Align to betty syle

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -6,13 +6,13 @@
  */
 void reverse_array(int *a, int n)
 {
-	int i;
-	int b;
-	for (i = 0; i < (n / 2); i++)
-	{
-		b = a[i];
-		a[i] = a[n - i - 1];
-		a[n - i - 1] = b;
-	}
+int i;
+int b;
+for (i = 0; i < (n / 2); i++)
+{
+b = a[i];
+a[i] = a[n - i - 1];
+a[n - i - 1] = b;
+}
 }
 
