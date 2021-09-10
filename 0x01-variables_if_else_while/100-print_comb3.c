29abc40d8c91d440507929ab957eda41d95c9734
Igweamaka Chigozirim <iamokwukwe@gmail.com>
2021-09-10 16:27:44 -0700

    Correct code in file 100

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -13,11 +13,11 @@ for (i = 0; i <= 9; i++)
 {
 for (j = 1; j <= 9; j++)
 {
-if (i != j)
+if (i < j)
 {
 putchar(i + '0');
 putchar(j + '0');
-if (i < 9)
+if (i < 8)
 {
 putchar(',');
 putchar(' ');
