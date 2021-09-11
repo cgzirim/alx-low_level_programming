578e8c54affc000dc30b1656319eb1a3cc2a700d
Igweamaka Chigozirim <iamokwukwe@gmail.com>
2021-09-11 06:38:51 -0700

    Correct file 9

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,10 +1,9 @@
 #include "main.h"
-
+#include <stdio.h>
 /**
  * times_table - Print the 9 times table, starting with 0
  * Return: 0
  */
-
 void times_table(void)
 {
 int x = 0; /* factor */
@@ -19,9 +18,10 @@ z = x * y;
 if (z > 9)
 {
 putchar(z / 10 + '0');
-putchar(z % 10 + '0')
+putchar(z % 10 + '0');
 }
-else if (y != 0)
+else
+if (y != 0)
 {
 putchar(' ');
 putchar(z + '0');
@@ -35,10 +35,10 @@ if (y != 9)
 putchar(',');
 putchar(' ');
 }
-y++:
+y++;
 }
 putchar('\n');
 x++;
 }
 }
- 
+
