bed1f14ce42b0a57457489033fc544ece58fe2a8
Igweamaka Chigozirim <iamokwukwe@gmail.com>
2021-09-11 02:58:07 -0700

    Redo task 102

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -6,38 +6,31 @@
  */
 int main(void)
 {
-	int a, b, c, d;
-	int i, j;
-	for (i = 0; i < 100; i++)
-	{
-		for (j = 1; j < 100 ; j++)
-		{
-			/* first num*/
-			a = i / 10;/* f digit*/
-			b = i % 10;/* s digit*/
-			/* second num*/
-			c = j / 10;/* f digit*/
-			d = j % 10;/* s digit*/
-			
-			if(b == d || d == b)
-			{
-				continue;
-			}
-			else
-			{
-			putchar(a + 48);
-			putchar(b + 48);
-			putchar(32);
-			putchar(c + 48);
-			putchar(d + 48);
-
-		/*	if (a != 9 && b != 8)*/
-			
-				putchar(44);
-				putchar(32);
-			}
-		}
-	}
-	putchar('\n');
-	return (0);
+int a, b, c, d;
+int i, j;
+for (i = 0; i < 100; i++)
+{
+for (j = 1; j < 100 ; j++)
+{
+/* first num*/
+a = i / 10;/* f digit*/
+b = i % 10;/* s digit*/
+/* second num*/
+c = j / 10;/* f digit*/
+d = j % 10;/* s digit*/			
+if (a < c || (a == c && b < d))
+{
+putchar(a + 48);
+putchar(b + 48);
+putchar(32);
+putchar(c + 48);
+putchar(d + 48);
+if (!(a == 9 && b == 8))			
+putchar(44);
+putchar(32);
+}
+}
+}
+putchar('\n');
+return (0);
 }
