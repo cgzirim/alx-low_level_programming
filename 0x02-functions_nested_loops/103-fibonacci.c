8c7594cd38adf42513ee5ebb518c607516b8106f
Igweamaka Chigozirim <iamokwukwe@gmail.com>
2021-09-11 20:00:23 -0700

    Redo task 103 of project 2

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -7,16 +7,28 @@
  */
 int main(void)
 {
-int i = 1, j = 2, total = 0;
-int k;
-while (j < 4000000)
+int count = 2;
+long int i = 1, j = 2;
+long int k;
+printf("%lu, ", i);
+while (count <= 4000000)
 {
 if (j % 2 == 0)
-total += j;
+{
+if (count == 50)
+{
+printf("%lu\n", j);
+}
+else
+{
+printf("%lu, ", j);
+}
+}
 k = j;
 j += i;
 i = k;
-}
-printf("%d\n", total);
+count++;
+}	
 return (0);
 }
+
