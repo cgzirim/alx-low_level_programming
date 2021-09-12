6c8f10a2034dc2f20cdab1fe9eb90c1963ebf1ae
Igweamaka Chigozirim <iamokwukwe@gmail.com>
2021-09-11 17:08:35 -0700

    Redo task 100 of project 2

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,46 +1,55 @@
 #include "main.h"
+#include <stdio.h>
 /**
- * print_times_table - Print the `n` times table, starting with 0.
- * Description: If `n` is greater than 15 or less than 0, print nothing.
- * @n: int type number
+ * print_times_table - Print the n times table, starting with 0
+ * Description: print nothing if n is greater than
+ * 15 or less than 0
+ * Return: 0
  */
 void print_times_table(int n)
 {
-int x = 0, y, z;
-if (n > 15 || n < 0)
-return;
+int x = 0; /* factor */
+int y; /* count  */
+int z; /* computed value */
+if (!(n > 15 || n < 0))
+{
 while (x <= n)
 {
-for (y = 0; y <= n; y++)
+y = 0;
+while (y <= n)
 {
 z = x * y;
 if (z > 99)
 {
-_putchar(z / 100 + '0');
-_putchar((z / 10 % 10) + '0');
-_putchar(z % 10 + '0');
+	putchar(z / 100 + '0');
+	putchar((z / 10 % 10) + '0');
+	putchar(z % 10 + '0');
 }
 else if (z > 9)
 {
-_putchar(' ');
-_putchar(z / 10 + '0');
-_putchar(z % 10 + '0');
+putchar(' ');
+putchar(z / 10 + '0');
+putchar(z % 10 + '0');
 }
 else if (y != 0)
 {
-_putchar(' ');
-_putchar(' ');
-_putchar(z + '0');
+putchar(' ');
+putchar(' ');
+putchar(z + '0');
 }
 else
-_putchar(z + '0');
+{
+putchar(z + '0');
+}
 if (y != n)
 {
-_putchar(',');
-_putchar(' ');
+putchar(',');
+putchar(' ');
 }
+y++;
 }
-_putchar('\n');
+putchar('\n');
 x++;
+}
 }
 }
