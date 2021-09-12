b495bc68ce58929e2c6b14cb5517e33aa4ee0884
Igweamaka Chigozirim <iamokwukwe@gmail.com>
2021-09-11 18:44:46 -0700

    Redo task 102 of project 2

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -7,8 +7,8 @@
  */
 int main(void)
 {
-int i = ;
-int n1 = 1, n2 = 2, n3;
+long int i = 1;
+long int n1 = 1, n2 = 2, n3;
 printf("%lu", n1);
 while (i <=50)
 {
@@ -16,9 +16,9 @@ n3 = n1 + n2;
 printf("%lu" n3);
 n1 = n2;	
 n2 = n3;
-i++;
-if (n < 50)
+if (i < 50)
 	printf(",\ ");
+i++
 }
 return (0);
 }
