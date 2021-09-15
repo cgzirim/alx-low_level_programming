2ff90a0933cda02687f0dc450a7bc4841f222944
Igweamaka Chigozirim <iamokwukwe@gmail.com>
2021-09-15 09:38:12 -0700

    Write a function that returns 1 if the input integer is a prime number, otherwise return 0.

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -12,7 +12,7 @@ int is_prime_number(int n);
 int is_divisible(int num, int div)
 {
 	if (num % div == 0)
-		return (0);	
+		return (0);
 	if (div == num / 2)
 		return (1);
 	return (is_divisible(num, div + 1));
@@ -28,6 +28,7 @@ int is_divisible(int num, int div)
 int is_prime_number(int n)
 {
 	int div = 2;
+	
 	if (n <= 1)
 		return (0);
 	if (n >= 2 && n <= 3)
