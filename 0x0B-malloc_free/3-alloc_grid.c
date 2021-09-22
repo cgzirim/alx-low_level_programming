b5c3ac743eda5f2310bb6ad2788b6c66cd775155
Igweamaka Chigozirim <iamokwukwe@gmail.com>
2021-09-22 03:51:57 -0700

    Understand concept and rewrite code in file 3

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -16,20 +16,22 @@ int **alloc_grid(int width, int height)
 	if (width <= 0 || height <= 0)
 		return (NULL);
 
-	grid = (void *)malloc(height * sizeof(*grid));
+	grid = (int **)malloc(height * sizeof(int *));
 	if (grid == NULL)
+	{
 		return (NULL);
+	}
 
 	for (i = 0; i < height; i++)
 	{
-		grid[i] = (void *)malloc(height * sizeof(**grid));
+		grid[i] = (int *)malloc(width * sizeof(int));
 		if (grid == NULL)
 		{
-			for (i--; i >= 0; i--)
-				free(grid[i]);
-			free(grid);
 			return (NULL);
 		}
+	}
+	for (i = 0; i < height; i++)
+	{
 		for (j = 0; j < width; j++)
 		{
 			grid[i][j] = 0;
