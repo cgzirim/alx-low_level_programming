ef2fa6c8c175975dc86c6b12f9e7643adf7cbdee
Igweamaka Chigozirim <iamokwukwe@gmail.com>
2021-09-22 05:31:57 -0700

    Align code to betty style

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -3,12 +3,13 @@
 #include <stdlib.h>
 /**
  * free_grid - Frees 2 dimensional array.
- * @**grid: 2d array to free.
+ * @grid: 2d array to free.
  * @height: Height of 2d array in type int.
  */
 void free_grid(int **grid, int height)
 {
 	int i;
+
 	if (grid == NULL || height <= 0)
 		return;
 
