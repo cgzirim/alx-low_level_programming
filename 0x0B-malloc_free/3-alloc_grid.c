9dbc531d674131f0e66b5e10f4cc268866daeba9
Igweamaka Chigozirim <iamokwukwe@gmail.com>
2021-09-22 04:23:42 -0700

    Free allocated memory

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -19,14 +19,18 @@ int **alloc_grid(int width, int height)
 	grid = (int **)malloc(height * sizeof(int *));
 	if (grid == NULL)
 	{
+		free(grid);
 		return (NULL);
 	}
 
 	for (i = 0; i < height; i++)
 	{
 		grid[i] = (int *)malloc(width * sizeof(int));
-		if (grid == NULL)
+		if (grid[i] == NULL)
 		{
+			free(grid);
+			for (j = 0; j < i; j++)
+				free(grid[j]);
 			return (NULL);
 		}
 	}
