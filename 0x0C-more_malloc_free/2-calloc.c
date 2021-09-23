bf2342322d7d3ee4d959e8733f85e52da69d9588
Igweamaka Chigozirim <iamokwukwe@gmail.com>
2021-09-23 15:27:40 -0700

    Fix bug in for loop

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -20,7 +20,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	if (ar == NULL)
 		return (NULL);
 
-	for (i = 0; i < (size * nmemb); i++)
+	for (i = 0; i < nmemb; i++)
 		ar[i] = 0;
 
 	arr = (void *)ar;
