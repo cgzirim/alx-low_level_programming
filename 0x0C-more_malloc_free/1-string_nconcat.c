94edf8bc809b7c6d59fcc4bdb7e7f07ff5c3da1e
Igweamaka Chigozirim <iamokwukwe@gmail.com>
2021-09-23 13:38:33 -0700

    Write a function that concatenates two strings.

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -25,7 +25,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	if (str == NULL)
 		return (NULL);
 
-	for (i = 0; s1[i] != '\0'; i++)
+	for (i = 0; s1[i]; i++)
 		str[i] = s1[i];
 
 	for (j = 0; j < n; j++, i++)
