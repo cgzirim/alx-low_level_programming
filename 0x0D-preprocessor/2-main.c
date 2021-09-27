b6946a9c4e375297cce94fb5e21f5180c3480cfb
Igweamaka Chigozirim <iamokwukwe@gmail.com>
2021-09-27 08:05:02 -0700

    Add README.md. 0. Create a header file that defines a macro named SIZE as an abbreviation for the token 1024. 1. Create a header file that defines a macro named PI as an abbreviation for the token 3.14159265359. 2. Write a program that prints the name of the file it was compiled from, followed by a new line. 3.  Write a function-like macro ABS(x) that computes the absolute value of a number x. 4. Write a function-like macro SUM(x, y) that computes the sum of the numbers x and y

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - Prints the file name of a program.
+ * Return: Always nothing.
+ */
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
+
---
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -0,0 +1,6 @@
+#ifndef ABS
+
+#define ABS(A) A < 0 ? -(A) : A
+
+#endif
+
