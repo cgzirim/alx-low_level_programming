8ee9d5b7318456de34bf3c771b3a71b8e393ff2e
Igweamaka Chigozirim <iamokwukwe@gmail.com>
2021-09-27 14:01:04 -0700

    Write a function that prints a struct dog

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -15,5 +15,8 @@ struct dog
 };
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+struct dog_t *new_dog(char *name, float age, char *owner);
+/* void free_dog(dog_t *d);*/
 #endif/* ifndef DOG_H*/
 
