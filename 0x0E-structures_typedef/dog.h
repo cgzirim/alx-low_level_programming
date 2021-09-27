fa3cd3bf43732ca5e4653e7135357de342aad415
Igweamaka Chigozirim <iamokwukwe@gmail.com>
2021-09-27 11:28:09 -0700

    Define a new type struct dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - A dog's basic information.
+ * @name: Dog's name.
+ * @owner: Owner's name.
+ * @age: Dog's age.
+ * Description: Longer description.
+ */
+struct dog
+{
+	char *name;
+	char *owner;
+	float age;
+};
+#endif/* ifndef DOG_H*/
+
