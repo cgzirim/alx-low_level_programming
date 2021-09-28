3eeb5e224fb9b7529842d57b8f4af713577d00fa
Igweamaka Chigozirim <iamokwukwe@gmail.com>
2021-09-27 17:01:34 -0700

    Write a function that creates a new dog.

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,46 @@
+#include "dog.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+/**
+ * new_dog - Cunction creates a new dog.
+ * @name: Name of dog.
+ * @age: Age of dog.
+ * @owner: Owner of dog.
+ * Return: New struct dog.
+ */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *dog;
+
+	if (!name || age < 0 || !owner)
+		return (NULL);
+
+	dog = malloc(sizeof(dog_t));
+	if (dog == NULL)
+	{
+		return (NULL);
+	}
+
+	dog->name = malloc(sizeof(char) * (strlen(name) + 1));
+		if (dog->name == NULL)
+		{
+			free(dog->name);
+			return (NULL);
+		}
+
+	dog->owner = malloc(sizeof(char) + (strlen(owner) + 1));
+		if (dog->owner == NULL)
+		{
+			free(dog->owner);
+			free(dog);
+			return (NULL);
+		}
+
+	dog->name = strcpy(dog->name, name);
+	dog->age = age;
+	dog->owner = strcpy(dog->owner, owner);
+
+	return (dog);
+}
+
