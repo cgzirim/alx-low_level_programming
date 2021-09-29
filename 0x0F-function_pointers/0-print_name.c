03d9de6ba200f5c9261ebfdc32dc8c6eca044058
Igweamaka Chigozirim <iamokwukwe@gmail.com>
2021-09-29 15:12:44 -0700

    Align code in file 0-print_name.c to betty style

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,10 +1,10 @@
 #include "function_pointers.h"
-
 /**
  * print_name - Function prints a name.
  * @name: Name to print.
  * @f: Callback function.
  */
+
 void print_name(char *name, void (*f)(char *))
 {
 	if (name == NULL || f == NULL)
