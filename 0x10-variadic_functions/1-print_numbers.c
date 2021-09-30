33c4b42df8bc22cb4aec3638d2eb1447c309699d
Igweamaka Chigozirim <iamokwukwe@gmail.com>
2021-09-30 13:54:48 -0700

    Remove condition for seperator != Null

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -13,19 +13,17 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	va_list nums;
 	unsigned int i;
 
-	if (separator != NULL)
+	va_start(nums, n);
+
+	for (i = 0; i < n; i++)
 	{
-		va_start(nums, n);
+		printf("%d", va_arg(nums, int));
 
-		for (i = 0; i < n; i++)
-		{
-			printf("%d", va_arg(nums, int));
-			if (i != (n - 1) && separator != NULL)
-				printf("%s", separator);
-		}
-		putchar('\n');
-
-		va_end(nums);
+		if (i != (n - 1) && separator != NULL)
+			printf("%s", separator);
 	}
+	putchar('\n');
+
+	va_end(nums);
 }
 
