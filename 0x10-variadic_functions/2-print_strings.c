49f307cf709670defd1d0335787963c574848d17
Igweamaka Chigozirim <iamokwukwe@gmail.com>
2021-09-30 15:04:31 -0700

    Use else after if for the condition str == Null

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -19,10 +19,11 @@ void print_strings(const char *separator, const unsigned int n, ...)
 	for (i = 0; i < n; i++)
 	{
 		str = va_arg(strings, char*);
+
 		if (!str)
 			printf("(nil)");
-
-		printf("%s", str);
+		else
+			printf("%s", str);
 
 		if (i != (n - 1) && separator != NULL)
 			printf("%s", separator);
