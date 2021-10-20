c10106d00aea1e0342de16d1c8115661281e6a89
Chigozirim Igweamaka <iamokwukwe@gmail.com>
2021-10-20 05:58:11 -0700

    Attempt to resolve bug

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -17,7 +17,7 @@ int create_file(const char *filename, char *text_content)
 	for (len = 0; text_content[len] != '\0';)
 		len++;
 
-	o = open(filename, O_CREAT | O_RDWR| O_TRUNC, 0600);
+	o = open(filename, O_RDWR | O_CREAT | O_TRUNC,  0600);
 	if (o < 0)
 		return (-1);
 
