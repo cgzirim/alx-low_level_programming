79dfcb2cc3f61115e17dbb9c64b1564091f1f0c7
Chigozirim Igweamaka <iamokwukwe@gmail.com>
2021-10-19 22:59:18 -0700

    Attempt to debug code

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -8,7 +8,7 @@
  */
 int create_file(const char *filename, char *text_content)
 {
-	int len;
+	int len, o, w;
 
 	if (!filename)
 		return (-1);
@@ -16,13 +16,15 @@ int create_file(const char *filename, char *text_content)
 	for (len = 0; text_content[len] != '\0';)
 		len++;
 
-/*
- * o = open(filename, O_RDONLY | O_CREAT | O_TRUNC, 0600);
- */
+	o = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
+	if (o < 0)
+		return (-1);
 
-	creat(filename, 0600);
-	write(STDOUT_FILENO, text_content, len);
+	w = write(STDOUT_FILENO, text_content, len);
+	if (w != len)
+		return (0);
 
+	close(o);
 	return (1);
 }
 
