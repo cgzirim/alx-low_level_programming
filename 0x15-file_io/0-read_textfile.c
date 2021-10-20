fec28e6170a4873766c53a2f67c6dbf5fe2370ce
Chigozirim Igweamaka <iamokwukwe@gmail.com>
2021-10-19 21:16:52 -0700

    Attempt to debug code

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,7 +1,5 @@
 #include "main.h"
 #include <stdlib.h>
-#include <stdio.h>
-#include <unustd.h>
 /**
  * read_textfile - Reads a text file and prints
  * it to the POSIX standard output.
@@ -20,9 +18,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 
 	o = open(filename, O_RDONLY);
 	if (o < 0)
-	{
 		return (0);
-	}
 
 	buffer = malloc(sizeof(char) * letters);
 	if (!buffer)
@@ -34,10 +30,10 @@ ssize_t read_textfile(const char *filename, size_t letters)
 		free(buffer);
 		return (0);
 	}
-
+	buffer[r] = '\0';
 	close(o);
 
-	w = write(STDIN_FILENO, buffer, r);
+	w = write(STDOUT_FILENO, buffer, r);
 	if (w < 0)
 	{
 		free(buffer);
