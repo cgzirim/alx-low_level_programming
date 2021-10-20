42d6870751b11c4cf1083f10218981780d5c0e8a
Chigozirim Igweamaka <iamokwukwe@gmail.com>
2021-10-19 22:13:26 -0700

    Try to write code right

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -8,7 +8,7 @@
  */
 int create_file(const char *filename, char *text_content)
 {
-	int len, o;
+	int len;
 
 	if (!filename)
 		return (-1);
@@ -16,12 +16,13 @@ int create_file(const char *filename, char *text_content)
 	for (len = 0; text_content[len] != '\0';)
 		len++;
 
-	o = open(filename, O_RDONLY | O_CREAT | O_TRUNC, 0600);
+/*
+ * o = open(filename, O_RDONLY | O_CREAT | O_TRUNC, 0600);
+ */
 
-	text_content[len] = '\0';
+	creat(filename, 0600);
 	write(STDOUT_FILENO, text_content, len);
 
-	close(o);
 	return (1);
 }
 
