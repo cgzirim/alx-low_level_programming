5e61377cb85edc643a5e0d5145a7e46634162a31
Chigozirim Igweamaka <iamokwukwe@gmail.com>
2021-10-20 09:39:06 -0700

    Write data into created filr, not in stdout

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -11,18 +11,21 @@ int create_file(const char *filename, char *text_content)
 {
 	int len, o, w;
 
-	if (!filename || !text_content)
+	if (!filename)
 		return (-1);
 
-	for (len = 0; text_content[len] != '\0';)
+	if (text_content)
+	{
+		for (len = 0; text_content[len] != '\0';)
 		len++;
+	}
 
 	o = open(filename, O_RDWR | O_CREAT | O_TRUNC,  0600);
 	if (o < 0)
 		return (-1);
 
 
-	w = write(STDOUT_FILENO, text_content, len);
+	w = write(o, text_content, len);
 	if (w != len)
 		return (-1);
 
