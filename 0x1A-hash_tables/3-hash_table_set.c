45812a62a8b94e365d4934697b6e0b17f6fc83db
Chigozirim Igweamaka <iamokwukwe@gmail.com>
2021-12-17 07:33:28 -0800

    Attempt to fix bug

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -14,10 +14,10 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	hash_node_t *node, *last;
 	unsigned long int index;
 
-	/*if (strlen(key) == 0 || strlen(value) == 0)
+	if (strlen(key) == 0 || strlen(value) == 0)
 		return (0);
 
-	if (!ht)
+	/* if (!ht)
 	return (0);*/
 
 	node = malloc(sizeof(hash_node_t));
