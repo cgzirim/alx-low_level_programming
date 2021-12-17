1eb6d60af8c6920488b23e0d683fe8bdb9b58af6
Chigozirim Igweamaka <iamokwukwe@gmail.com>
2021-12-17 05:55:53 -0800

    Align code to Betty style

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -11,38 +11,38 @@
  */
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-  hash_node_t *node, *last;
-  unsigned long int index;
+	hash_node_t *node, *last;
+	unsigned long int index;
 
-  if (strlen(key) == 0 || strlen(value) == 0)
-    return(0);
+	if (strlen(key) == 0 || strlen(value) == 0)
+		return (0);
 
-  if (!ht)
-    return (0);
+	if (!ht)
+		return (0);
 
-  node = malloc(sizeof(hash_node_t));
-  if (!node)
-    return (0);
+	node = malloc(sizeof(hash_node_t));
+	if (!node)
+		return (0);
 
-  node->key = strdup(key);
-  node->value = strdup(value);
-  node->next = NULL;
+	node->key = strdup(key);
+	node->value = strdup(value);
+	node->next = NULL;
 
-  /* Get index for element key.*/
-  index = key_index((const unsigned char *)key, ht->size);
-  if (!index)
-    return (0);
+	/* Get index for the element key.*/
+	index = key_index((const unsigned char *)key, ht->size);
+	if (!index)
+		return (0);
 
-  /* Input pointer to node at index.*/
-  if (ht->array[index] == NULL)
-    ht->array[index] = node;
-  else
-    {
-      last = ht->array[index];
-      while (last->next != NULL)
-	last = last->next;
-      last->next = node;
-    }
+	/* Input pointer to node at index.*/
+	if (ht->array[index] == NULL)
+		ht->array[index] = node;
+	else
+	{
+		last = ht->array[index];
+		while (last->next != NULL)
+			last = last->next;
+		last->next = node;
+	}
 
-  return (1);
+	return (1);
 }
