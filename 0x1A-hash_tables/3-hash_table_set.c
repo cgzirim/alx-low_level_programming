539403b68f4d4ba47742d162064d48c439432a90
Chigozirim Igweamaka <iamokwukwe@gmail.com>
2021-12-19 04:32:53 -0800

    Function should replace replace value of key if key already exist.

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -12,37 +12,40 @@
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	hash_node_t *node;
-	unsigned long int index;
+	unsigned long int index, i;
 	char *value_copy;
 
 	if (!ht || !key || *key == '\0' || !value)
 		return (0);
 
+	value_copy = strdup(value);
+	if (!value_copy)
+		return (0);
+
+	index = key_index((const unsigned char *)key, ht->size);
+
+	for (i = index; ht->array[i]; i++)
+	{
+		if (strcmp(ht->array[i]->key, key) == 0)
+		{
+			free(ht->array[i]->value);
+			ht->array[i]->value = value_copy;
+			return (1);
+		}
+	}
+
 	node = malloc(sizeof(hash_node_t));
 	if (!node)
 		return (0);
 
-	value_copy = strdup(value);
-	if (!value_copy)
-	  {
-		  free(node);
-		  return (0);
-	  }
 	node->key = strdup(key);
 	if (!node->key)
 	{
 		free(node);
 		return (0);
 	}
-	node->value = value_copy;
 
-	/* Get index for the element key.*/
-	index = key_index((const unsigned char *)key, ht->size);
-	if (!index)
-	{
-		free(node);
-		return (0);
-	}
+	node->value = value_copy;
 
 	node->next = ht->array[index];
 	ht->array[index] = node;
