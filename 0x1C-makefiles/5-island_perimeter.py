0c817b0e5b58e1c201d5e0af228eabee6d310cba
Chigozirim Igweamaka <iamokwukwe@gmail.com>
2022-02-17 15:29:54 -0800

    Correct error.

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -5,30 +5,17 @@
 def island_perimeter(grid):
     """Returns the perimeter of the island described in grid."""
 
-    cell_perimeter = 0
-    island_perimeter = 0
-    row_idx = 0
+    width = len(grid[0])
+    height = len(grid)
+    edges = 0
+    size = 0
 
-    for row in grid:
-        cell_idx = 0
-        for cell in row:
-            if cell == 1:
-                # Getting the perimeter of a cell:
-                # Check the north
-                if grid[row_idx - 1][cell_idx] == False:
-                    cell_perimeter += 1
-                # Check the east
-                if row[cell_idx + 1] == False:
-                    cell_perimeter += 1
-                # Check the south
-                if grid[row_idx + 1][cell_idx] == False:
-                    cell_perimeter += 1
-                # Check the west
-                if row[cell_idx - 1] == False:
-                    cell_perimeter += 1
-
-            cell_idx += 1
-        island_perimeter += cell_perimeter
-        cell_perimeter = 0
-        row_idx += 1
-    return island_perimeter
+    for i in range(height):
+        for j in range(width):
+            if grid[i][j] == 1:
+                size += 1
+                if (j > 0 and grid[i][j - 1] == 1):
+                    edges += 1
+                if (i > 0 and grid[i - 1][j] == 1):
+                    edges += 1
+    return size * 4 - edges * 2
