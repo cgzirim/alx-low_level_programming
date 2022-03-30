6d192742075750323aab05a5a033b97cd970cc57
root <iamokwukwe@gmail.com>
2022-03-30 12:14:14 -0700

    Align code to Betty style

--- 0x1E-search_algorithms/100-jump.c
+++ 0x1E-search_algorithms/100-jump.c
@@ -7,48 +7,49 @@
  * @array: Pointer to the first element of the array to search in.
  * @size: The number of elementes in array.
  * @value: The value to search for.
- * 
- * Returns: On success -  first index where value is located. Otherwise, -1.
+ *
+ * Return: On success -  first index where value is located. Otherwise, -1.
  */
 int jump_search(int *array, size_t size, int value)
 {
-    size_t n = size - 1;
-    size_t jump = sqrt(n);
-    size_t lst_jmp = 0, i;
+	size_t n = size - 1;
+	size_t jump = sqrt(n);
+	size_t lst_jmp = 0, i;
 
-    if (array == NULL)
-        return (-1);
 
-    while (jump <= n + sqrt(n))
-    {
-        printf("Value checked array[%ld] = [%d]\n", lst_jmp, array[lst_jmp]);
-        
-        if (array[jump] >= value)
-        {
-            printf("Value found between indexes [%ld] and [%ld]\n", lst_jmp, jump);
-            
-            for (i = lst_jmp; i <= jump; i++)
-            {
-                printf("Value checked array[%ld] = [%d]\n", i, array[i]);
+	if (array == NULL || size == 0)
+		return (-1);
 
-                if (array[i] == value)
-                    return (i);
-            }
-        }
+	while (jump <= n + sqrt(n))
+	{
+		printf("Value checked array[%ld] = [%d]\n", lst_jmp, array[lst_jmp]);
 
-        if (jump >= n + sqrt(n))
-        {
-            printf("Value found between indexes [%ld] and [%ld]\n", lst_jmp, jump);
-            for (i = lst_jmp; i <= n; i++)
-            {
-                printf("Value checked array[%ld] = [%d]\n", i, array[i]);
+		if (array[jump] >= value)
+		{
+			printf("Value found between indexes [%ld] and [%ld]\n", lst_jmp, jump);
 
-                if (array[i] == value)
-                    return (i);
-            }
-        }
-        lst_jmp = jump;
-        jump += sqrt(n);
-    }
-    return (-1);
+			for (i = lst_jmp; i <= jump; i++)
+			{
+				printf("Value checked array[%ld] = [%d]\n", i, array[i]);
+
+				if (array[i] == value)
+					return (i);
+			}
+		}
+
+		if (jump >= n + sqrt(n))
+		{
+			printf("Value found between indexes [%ld] and [%ld]\n", lst_jmp, jump);
+			for (i = lst_jmp; i <= n; i++)
+			{
+				printf("Value checked array[%ld] = [%d]\n", i, array[i]);
+
+				if (array[i] == value)
+					return (i);
+			}
+		}
+		lst_jmp = jump;
+		jump += sqrt(n);
+	}
+	return (-1);
 }
